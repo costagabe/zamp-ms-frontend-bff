2cad814dc9e8127db1fe2df200ed7170c15102d5
gabrielcosta <devgabcosta@gmail.com>
2026-03-07 21:12:14 -0300

    chore: remove Tailwind CSS module, add Zod integration, and create database types

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,6 @@ export default defineNuxtConfig({
     "@nuxt/hints",
     "@nuxtjs/supabase",
     "@vee-validate/nuxt",
-    "@nuxtjs/tailwindcss",
   ],
   devtools: { enabled: true },
   compatibilityDate: "2024-04-03",
